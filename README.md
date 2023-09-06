281bb4542dc154576571a1016a96ba4d065236c1
Zarina Uvalieva <zarina.uvalievaa@gmail.com>
2023-09-06 11:23:28 +0600

    Model

---
+++ README.md
@@ -0,0 +1,2 @@
+# kyrgyz-punctuation-model
+This is a kyrgyz-punctuation-model that trained on xlm-roberta-base pre-trained model
---
+++ pipeline.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f7a71fec91811f3ce816537dcd3052f202a19a90a92236885465dea4d9246ed
+size 861
